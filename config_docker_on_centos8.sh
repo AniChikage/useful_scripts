ec9d43e1f6aee6ebcba2befc3137a10065fad5ed
AniChikage <anichikage@163.com>
2021-09-09 15:46:17 +0800

    Create config_docker_on_centos8.sh

---
+++ config_docker_on_centos8.sh
@@ -0,0 +1,5 @@
+curl https://download.docker.com/linux/centos/docker-ce.repo -o /etc/yum.repos.d/docker-ce.repo
+yum install -y https://download.docker.com/linux/fedora/30/x86_64/stable/Packages/containerd.io-1.2.6-3.3.fc30.x86_64.rpm
+yum install -y docker-ce
+
+
