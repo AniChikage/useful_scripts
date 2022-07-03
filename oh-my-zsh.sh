00a7e97c3c62e6c36f96a015422d43b976333266
AniChikage <anichikage@163.com>
2022-07-03 11:54:39 +0800

    Create oh-my-zsh.sh

---
+++ oh-my-zsh.sh
@@ -0,0 +1,20 @@
+!/bin/bash
+
+yum install -y zsh
+yum install -y git
+yum install -y wget
+
+wget https://gitee.com/mirrors/oh-my-zsh/raw/master/tools/install.sh
+chmod +x install.sh
+sh install.sh
+
+cd ~/.oh-my-zsh
+git remote set-url origin https://gitee.com/mirrors/oh-my-zsh.git
+git pull
+
+dnf install -y util-linux-user
+chsh -s /bin/zsh
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+
